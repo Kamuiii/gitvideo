16101ea89b705cd9fcc797b7f4d7a14c9b59e8cf
Fabio <fabio.schneider02@gmx.ch>
2024-03-10 22:23:29 +0100

    tried to overwrite everything

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,16 @@
 print("this is a test")
+
+print(
+    "now we can change a lot here and when i try to upload this again the changes will automatically go onto github"
+)
+
+
+# here we gotta make some comments in order to keep this all clean
+
+# we define x as 10
+
+x = 10
+print(2 * x)
+
+
+# now we gotta commit
